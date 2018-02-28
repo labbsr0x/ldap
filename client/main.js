f0cd2df9792f8c6ce3557186fd2f86ee87faa86e
thiagofss <f9329476@bb.com.br>
2018-02-28 12:24:17 -0300

    experimentações

--- client/main.js
+++ client/main.js
@@ -3,14 +3,27 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.index.onCreated(function indexOnCreated() {
+
+  this.currentUser = new ReactiveVar('teste');
+
+});
+
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
+  },
+});
+
+Template.index.helpers({
+    currentUser() {
+    return Template.instance().currentUser.get();
   },
 });
 
